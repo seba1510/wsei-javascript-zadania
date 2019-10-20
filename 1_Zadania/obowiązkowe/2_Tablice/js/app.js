b5a684a3ab73fcbaf01ae9bef30301cb75f77509
seba1510 <48321015+seba1510@users.noreply.github.com>
2019-10-20 11:08:51 +0200

    Update app.js

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -30,4 +30,51 @@ function printTable(array) {
     }  
 }
 
-//
+//Zadanie 4
+
+function multiply(array) {
+    var temp = 1;
+    for(let i=0; i<array.length; i++){
+        temp *= array[i];
+    }
+    console.log(temp);
+}
+
+//Zadanie 5
+
+function getEvenAverage(array){
+    let arg = 0;
+    let evennumber = 0;
+    array.forEach(element => {
+        if(element %2==0){
+        arg+=element;
+        evennumber++;
+        }
+    });
+    console.log(arg/evennumber);
+}
+console.log(getEvenAverage ([1,2,3,4,5,6,7,8]));
+
+//Zadanie 6
+
+function sortArray(array){
+var tab = array;
+tab.sort((a,b)=>a-b);
+return(array);
+}
+console.log(sortArray([12,5,3,8,10]));
+
+//Zadanie 7
+
+function Addarrays(array, array2){
+    var counter = array.length > array2.length?array.length:array2.length;
+   var result = [];
+   for(var i =0;i<counter;i++){
+       var firstNum=array[i]!=null?array[i]:0;
+       var secondNum=array2[i]!=null?array2[i]:0;
+       result[i] = firstNum + secondNum;
+   }
+   return result;
+}
+Addarrays([1,2,3,4],[2,1,6,7]);
+
