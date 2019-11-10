e82b61d7a98cc9695caa9ef87bf0d3e44b0b7034
seba1510 <48321015+seba1510@users.noreply.github.com>
2019-11-10 10:43:06 +0100

    Update app.js

---
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -0,0 +1,22 @@
+//Zadanie 0a
+city = {
+"capital":"string",
+"population":"number", 
+"president":"string", 
+"primeMinister": "tablica stringów", };
+console.log(city);
+
+//Zadanie 0b
+timeMachine={"shape": "string", "model": "string", run(date,place)};
+console.log(timeMachine);
+
+//Zadanie 1
+book = {"title":"" , "author":"", "numberOfPages":""};
+console.log(book);
+
+//Zadanie 2
+person={"name":"", "age":"", sayHello() };
+function sayHello(){
+    console.log("hello");
+}
+console.log(person);
