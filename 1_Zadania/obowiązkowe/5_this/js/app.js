3347f8000f5d343b982f48e0f22b39d34732dc69
seba1510 <48321015+seba1510@users.noreply.github.com>
2019-11-24 10:20:11 +0100

    Update app.js

---
+++ 1_Zadania/obowiązkowe/5_this/js/app.js
@@ -0,0 +1,70 @@
+//Zadanie 0
+var car = {
+    brand: "bmw", color: "blue", numberOfKilometers: 0, printCarinfo() {
+        console.log(this.brand);
+        console.log(this.color);
+        console.log(this.numberOfKilometers);
+    }, drive(km){
+        this.numberOfKilometers+=km;
+        console.log(this.numberOfKilometers);
+    }
+};
+car.printCarinfo();
+car.drive(10);
+
+//Zadanie 1
+var car = {
+    brand: "bmw", color: "blue", numberOfKilometers: 0, printCarinfo() {
+        console.log(this.brand);
+        console.log(this.color);
+        console.log(this.numberOfKilometers);
+    }, drive(km){
+        this.numberOfKilometers+=km;
+        console.log(this.numberOfKilometers);
+    }, add(dane){
+        tab.push(dane);
+    }, return1(){
+        for(let i = 0; i<tab.length; i++){
+            console.log(tab[i]);
+        }
+    }  
+
+     
+};
+var tab = [];
+car.add(1992);
+car.add(1993);
+car.add(1995);
+car.return1();
+
+//Zadanie 2
+var calculator = {
+    save: function (newA, newB) {
+        this.a = newA;
+        this.b = newB;
+        }, 
+    sum: function() {
+        var sum;
+        sum = this.a + this.b;
+        return sum;
+        },
+     multiply: function() {
+        var multiply;
+        multiply = this.a * this.b;
+         return multiply;
+        }
+};
+
+calculator.save(2, 3);
+console.log(calculator.sum());
+console.log(calculator.multiply());
+
+
+
+
+
+
+
+
+
+
