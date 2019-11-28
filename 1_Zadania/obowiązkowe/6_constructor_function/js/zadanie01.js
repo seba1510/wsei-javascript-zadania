933ebeacdf686e87fba2ed4598bfbc60bd55989b
seba1510 <48321015+seba1510@users.noreply.github.com>
2019-11-28 13:24:42 +0100

    Update zadanie01.js

--- 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
@@ -1,23 +1,31 @@
-var Robot = function (name) {
+var Robot = function(name) {
     this.name = name;
     this.isFunctional = true;
 }
 
 Robot.prototype.sayHi = function (toWho) {
-    if (isFunctional === true) {
-        console.log("Robot " + this.name + " greets " + toWho);
+    if (this.isFunctional == true) {
+        console.log("Robot " + name + " greets " + toWho);
     }
     else {
-        console.log("Robot " + this.name + "is broken");
+        console.log("Robot " + name + " is broken");
     }
 };
 
 Robot.prototype.changeName = function (newname) {
-    console.log("Robot " + this.name + "changes name to " + newname);
+    console.log("Robot " + name + " changes name to " + newname);
     name = newname;
 };
 
 Robot.prototype.fixIt = function () {
     this.isFunctional = true;
-    console.log("Robot " + this.name + "was fixed");
-};
+    console.log("Robot " + name + " was fixed");
+}
+
+Robot1 = new Robot("Janusz");
+Robot.prototype.sayHi("Michael");
+Robot.prototype.changeName("Kozak");
+Robot.prototype.fixIt();
+Robot.prototype.sayHi("Michael");
+
+
