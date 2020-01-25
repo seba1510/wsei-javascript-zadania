a173cd06cf52c7001e8aa6707432d1b0064f1d8f
seba1510 <48321015+seba1510@users.noreply.github.com>
2020-01-25 21:32:50 +0100

    Update zadanie06.js

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie06.js
@@ -0,0 +1,12 @@
+<script>
+document.addEventListener("DOMContentLoaded",fooo);
+function fooo(){
+document.getElementsByClassName("box big")[0].addEventListener("mousemove",function(){
+document.getElementById("globalX").innerHTML=event.pageX;
+document.getElementById("globalY").innerHTML=event.pageY;
+document.getElementById("localX").innerHTML=event.clientX-this.offsetLeft;
+document.getElementById("localY").innerHTML=event.clientY-this.offsetTop;});
+  }
+  
+  
+</script>
