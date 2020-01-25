1ae60147595ecc6db1da9aaca6bba730de7f5b3c
seba1510 <48321015+seba1510@users.noreply.github.com>
2020-01-25 21:53:47 +0100

    Update zadanie04.js

---
+++ 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie04.js
@@ -0,0 +1,11 @@
+<script>
+document.getElementById("remove").addEventListener("click",foo);
+function foo()
+{
+var fo=document.getElementsByClassName("list")[0];
+fo.removeChild(fo.children[0])
+fo.removeChild(fo.children[0])
+fo.removeChild(fo.children[0])
+fo.removeChild(fo.children[0])
+}
+</script>
