aacef8fa16bf1d54f4a1800d37abc7b073e5c111
seba1510 <48321015+seba1510@users.noreply.github.com>
2020-01-25 21:36:16 +0100

    Update zadanie08.js

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie08.js
@@ -0,0 +1,12 @@
+<script>
+document.addEventListener("DOMContentLoaded",fooo);
+document.getElementById("windowWidth").innerHTML=window.innerHeight;
+document.getElementById("windowHeight").innerHTML=window.innerWidth;
+function fooo()
+{
+    window.addEventListener("resize",function(){
+    document.getElementById("windowWidth").innerHTML=window.innerHeight;
+    document.getElementById("windowHeight").innerHTML=window.innerWidth;
+});
+}
+</script>
