24580c392333585c47946bb49c97f5dc2155dec0
seba1510 <48321015+seba1510@users.noreply.github.com>
2020-01-25 20:51:49 +0100

    Update zadanie01.js

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -3,3 +3,6 @@ var paragraf = document.querySelector("p");
 
 menu.classList.add("menu");
 paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+
+//DOMContentLoaded pokazuje czy element został wczytany.
+//bez dodania eventu, skrypt pracuje na dokumencie który jest nie wczytany - błąd.
